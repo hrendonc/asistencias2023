7f7baeaa79b4d95cee77a91f6e952004c02463e9
Hector Rendon <hrchilpo@gmail.com>
2023-11-23 14:41:17 -0600

    Cambios menores

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -7,14 +7,15 @@ function Auth() {
 
     return (
         <>
-            <label htmlFor="email">Correo electrónico</label>
+            {/* <label htmlFor="email">Correo electrónico</label>
             <input type="email" id='email' onChange={(e) => setEmail(e.target.value)} />
             <label htmlFor="pass">Contraseña</label>
             <input type="password" id='pass' onChange={(e) => setPass(e.target.value)} />
-            {/* <input type="button" onClick={() => accesoUsuario(email, pass)} value="Iniciar Sesión" />
+            <input type="button" onClick={() => accesoUsuario(email, pass)} value="Iniciar Sesión" />
             <input type="button" onClick={() => registraUsuario(email, pass)} value="Registrar" />
             <button onClick={() => registroGoogleRedirect()} >Entrar con google Redirect</button> */}
-            <button onClick={() => registroGooglePopup()} >Entrar con cuenta de Google</button>
+            <h3>Control de Registros de Asistencia</h3>
+            <button onClick={() => registroGooglePopup()} >Acceder con cuenta de Google</button>
         </>
     )
 }
